6383cc0832207f260fa1a768adef9fdc93043ff5
Britani A <72696767+BritaniAdi@users.noreply.github.com>
2022-03-16 13:22:33 +0700

    Add files via upload

---
+++ Praktik-1.py
@@ -0,0 +1,46 @@
+from traceback import print_tb
+import matplotlib.pyplot as plt
+print["Enter the value of x1:  "]
+x1 = int(input())
+print("Enter the value of x2:  ")
+x2 = int(input())
+print("Enter the value of y1:  ")
+y1 = int(input())
+print("Enter the value of y2:  ")
+y2 = int(input())
+
+dx = x2 - x1
+dy = y2 - y1
+
+if abs(dx) > abs(dy):
+    steps = abs(dx)
+else:
+    steps = abs(dy)
+
+xincrement = dy/steps
+yincrement = dy/steps 
+
+i = 0
+
+xcoordinates = []
+ycoordinates = []
+
+while i < steps:
+    i +=1
+    x1 = x1 + xincrement
+    y1 = y1 + yincrement
+    print("X1: ", x1, "y1: ", y1)
+    xcoordinates.append(x1)
+    ycoordinates.append(y1)
+
+plt.plot(xcoordinates, ycoordinates)
+
+#Naming the Axis
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+
+#Graph title 
+plt.title("DDA Algorithm")
+
+#Show the plot 
+plt.show
